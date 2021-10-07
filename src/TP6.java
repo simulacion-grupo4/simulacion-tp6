036e7a9334ae1e46972474e2b2dc8b6eae4b05dd
LucianaNapolillo <luciananapolillo@gmail.com>
2021-10-07 18:52:34 -0300

    argumentos

--- src/TP6.java
+++ src/TP6.java
@@ -5,9 +5,9 @@ public class TP6 {
     static final long TF = 2592000;                //Tiempo de simulacion de un mes
 
     // Control:
-    static final int NC = 5;                    //Cantidad de Cabinas
-    static final int NQ = NC * 6;                    //Numero de vehiculos para hacer quiebre
-    static final int TipoHora=3;					//Seleccionamos la banda horaria 
+    static int NC = 5;                    //Cantidad de Cabinas
+    static int NQ = NC * 6;                    //Numero de vehiculos para hacer quiebre
+    static int TipoHora=2;					//Seleccionamos la banda horaria
     
     // Datos:
     static long IA;                                //Intervalo entre arribos
@@ -45,6 +45,11 @@ public class TP6 {
 
 
     public static void main(String[] args) {
+        if (args.length == 3) {
+            NC = Integer.parseInt(args[0]);
+            NQ = Integer.parseInt(args[1]);
+            TipoHora = Integer.parseInt(args[2]);
+        }
         new TP6().simulacion();
     }
 
@@ -206,7 +211,7 @@ public class TP6 {
         if(TipoHora==1){
             ia = CalculoIA1();
         }
-        else if (TipoHora==2){
+        else if (TipoHora==3){
             //entre las 7am y las 21hs
             ia = CalculoIA2();
         } 
